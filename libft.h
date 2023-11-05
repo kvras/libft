188fdbdc4a75f987e4cd34beb73b97a232a2bad8
Kvras <iguiji.etudes@gmail.com>
2023-11-05 21:39:12 +0100

    makefile

--- libft.h
+++ libft.h
@@ -51,4 +51,17 @@ char	*ft_strtrim(char const *str, char const *set);
 char	*ft_substr(const char *str,int start,int len);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
+typedef struct t_list{
+    void    *content;
+    struct  s_list  *next;
+}t_list;
+t_list    *ft_lstnew(void *content);
+void    ft_lstadd_front(t_list **lst, t_list *new);
+int        ft_lstsize(t_list *lst);
+t_list    *ft_lstlast(t_list *lst);
+void    ft_lstdelone(t_list *lst, void (*del)(void*));
+void    ft_lstadd_back(t_list **lst, t_list *new);
+void    ft_lstclear(t_list **lst, void (*del)(void*));
+void    ft_lstiter(t_list *lst, void (*f)(void *));
+t_list    *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
